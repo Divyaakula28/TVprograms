f308d5744ba77468c772b412b3b40c474dfc677d
Akula Divya <65438113+Divyaakula28@users.noreply.github.com>
2021-04-12 17:23:54 +0530

    Update settings.py

--- tvschedule/settings.py
+++ tvschedule/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
