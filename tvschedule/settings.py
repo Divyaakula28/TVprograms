79c0908e16e580098b8b63314c46e57bf182bb50
Akula Divya <65438113+Divyaakula28@users.noreply.github.com>
2021-04-12 14:38:32 +0530

    Update settings.py

--- tvschedule/settings.py
+++ tvschedule/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -102,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
